a88f6b1c55e64896cc073544cdaa564c7bc31204
Qi-pipi <90182888+Qi-pipi@users.noreply.github.com>
2022-06-03 03:34:31 +0200

    Update test_process.py

--- test_process.py
+++ test_process.py
@@ -3,9 +3,8 @@ from process import create_test_dateset
 import unittest
 
 class TestStringMethods(unittest.TestCase):
-    # local testing
     def test_prediction(self):
-        self.assertEqual(create_train_dateset('data'),'/data/trainset.csv')
-        self.assertEqual(create_test_dateset('data'),'/data/testset.csv')
+        self.assertEqual(create_train_dateset('data'), '/data/trainset.csv')
+        self.assertEqual(create_test_dateset('data'), '/data/testset.csv')
 if __name__=="__main__":
     unittest.main()

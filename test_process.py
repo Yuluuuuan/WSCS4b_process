4a6f122d971969b242b95b6df7527d7104248eca
Qi-pipi <90182888+Qi-pipi@users.noreply.github.com>
2022-06-03 03:35:59 +0200

    Update test_process.py

--- test_process.py
+++ test_process.py
@@ -2,9 +2,12 @@ from process import create_train_dateset
 from process import create_test_dateset
 import unittest
 
+
 class TestStringMethods(unittest.TestCase):
     def test_prediction(self):
         self.assertEqual(create_train_dateset('data'), '/data/trainset.csv')
         self.assertEqual(create_test_dateset('data'), '/data/testset.csv')
+        
+ 
 if __name__=="__main__":
     unittest.main()
